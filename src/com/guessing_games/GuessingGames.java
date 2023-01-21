06f4bed2e40172bf9fb4f760f8075922a87d9e30
dev-suren <surenrana113@gmail.com>
2023-01-21 21:22:07 +0530

    this time i used math.random but next time i will use algo to generate random number

---
+++ src/com/guessing_games/GuessingGames.java
@@ -0,0 +1,32 @@
+package com.guessing_games;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessingGames {
+    private int randomizeNumbers(){
+        Random rand = new Random();
+        return rand.nextInt(100);
+
+    }
+
+    public void getRandomizeNumber(){
+        boolean condition = true;
+        int getNumber = randomizeNumbers();
+        while(condition == true){
+            Scanner newScanner = new Scanner(System.in);
+            int input = newScanner.nextInt();
+            if(input == getNumber){
+                System.out.println("you guessed right number");
+                condition = false;
+            } else if (input > getNumber ) {
+                System.out.println("guess lower");
+            }
+            else {
+                System.out.println("guess higher");
+            }
+        }
+    }
+
+
+}
---
+++ src/com/guessing_games/Main.java
@@ -0,0 +1,8 @@
+package com.guessing_games;
+
+public class Main {
+    public static void main(String[] args) {
+        GuessingGames obj = new GuessingGames();
+        obj.getRandomizeNumber();
+    }
+}
